2fc2aa4053dd4db0a25a55c285503d963457483e
Mihnea Simian <mihnea.simian@ing.ro>
2017-01-02 09:49:16 +0200

    large2str supports decimals

--- large2str.m
+++ large2str.m
@@ -2,7 +2,12 @@
 function result = large2str(x)
 %returns string out of large number
     result = "";
+    index = 0;
     for i = x.digits'
+        index++;
+        if (x.no_digits - x.no_decimals + 1 == index)
+            result = strcat(result, ".");
+        endif
         result = strcat(result, num2str(i));
     endfor
 endfunction
